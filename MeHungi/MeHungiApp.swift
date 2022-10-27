931d162e7b40bc43b88d476c4daf8e18e0bd096f
amccully <amccully2001@gmail.com>
2022-10-27 11:01:30 -0700

    Initial Commit

---
+++ MeHungi/MeHungiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeHungiApp.swift
+//  MeHungi
+//
+//  Created by Aaron McCully on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MeHungiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
